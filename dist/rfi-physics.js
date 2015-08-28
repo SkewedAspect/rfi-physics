2cbb6f5c1fbe931691aa88a1b83a2aa877918183
Christopher S. Case <chris.case@g33xnexus.com>
2015-08-27 20:34:36 -0500

    added built file.

--- dist/rfi-physics.js
+++ dist/rfi-physics.js
@@ -1,25 +1,37 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (process){
 //----------------------------------------------------------------------------------------------------------------------
-// A simple wrapper for performance.now. Falls back to Date.now if performance isn't available.
-//
-// @module performance.js
+/// A simple wrapper for performance.now. Falls back to Date.now if performance isn't available.
+///
+/// @module
 //----------------------------------------------------------------------------------------------------------------------
 
-module.exports = function()
+// Compatibility for using node's hrtime.
+function hrtimeMS()
 {
-    if(typeof window != 'undefined' && typeof window.performance != 'undefined'
-        && typeof window.performance.now != 'undefined')
-    {
-        return window.performance.now();
-    }
-    else
-    {
-        return Date.now();
-    } // end if
-};
+    var hr = process.hrtime();
+    return (hr[0] * 1e3) + (hr[1] / 1e6);
+} // end hrtimeMS
 
 //----------------------------------------------------------------------------------------------------------------------
-},{}],2:[function(require,module,exports){
+
+if(process && process.hrtime)
+{
+    module.exports = hrtimeMS;
+}
+else if(performance && performance.now)
+{
+    module.exports = performance.now;
+}
+else
+{
+    module.exports = Date.now;
+} // end if
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}).call(this,require('_process'))
+},{"_process":6}],2:[function(require,module,exports){
 //----------------------------------------------------------------------------------------------------------------------
 // Brief description for index.js module.
 //
@@ -27,7 +39,7 @@ module.exports = function()
 //----------------------------------------------------------------------------------------------------------------------
 
 var cannon = require('cannon');
-var now = require('performance-now');
+var now = require('./compat/performance');
 
 var PhysicsEngine = require('./lib/engine');
 var TargetVelocityController = require('./lib/targetvel');
@@ -53,7 +65,7 @@ if(typeof window != 'undefined')
 
 //----------------------------------------------------------------------------------------------------------------------
 
-},{"./lib/engine":3,"./lib/targetvel":4,"cannon":5,"performance-now":1}],3:[function(require,module,exports){
+},{"./compat/performance":1,"./lib/engine":3,"./lib/targetvel":4,"cannon":5}],3:[function(require,module,exports){
 //----------------------------------------------------------------------------------------------------------------------
 // Brief description for engine.js module.
 //
@@ -62,7 +74,7 @@ if(typeof window != 'undefined')
 
 var _ = require('lodash');
 var cannon = require('cannon');
-var now = require("performance-now");
+var now = require("../compat/performance");
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -189,7 +201,7 @@ module.exports = PhysicsEngine;
 
 //----------------------------------------------------------------------------------------------------------------------
 
-},{"cannon":5,"lodash":6,"performance-now":1}],4:[function(require,module,exports){
+},{"../compat/performance":1,"cannon":5,"lodash":7}],4:[function(require,module,exports){
 //---------------------------------------------------------------------------------------------------------------------
 /// Ship movement calculations based on target velocity.
 ///
@@ -11980,6 +11992,98 @@ World.prototype.internalStep = function(dt){
 });
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],6:[function(require,module,exports){
+// shim for using process in browser
+
+var process = module.exports = {};
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = setTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            currentQueue[queueIndex].run();
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    clearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        setTimeout(drainQueue, 0);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+// TODO(shtylman)
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
+
+},{}],7:[function(require,module,exports){
 (function (global){
 /**
  * @license
